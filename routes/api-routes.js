00e0d1a97730c3efa2bc26531225df9b44f99d93
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-07-26 22:53:01 +0100

    app.get("/api/workouts/range")

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
 const { Workout } = require("../models");
+const moment = require("moment");
 
 module.exports = (app) => {
   app.get("/api/workouts", async (req, res, next) => {
@@ -24,6 +25,19 @@ module.exports = (app) => {
   app.post("/api/workouts", async (req, res, next) => {
     try {
       res.json(await Workout.create({}));
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  app.get("/api/workouts/range", async (req, res, next) => {
+    const currentWeek = moment().isoWeek();
+    try {
+      const workouts = await Workout.find({});
+      // Filtering down to current weeks data
+      res.json(
+        workouts.filter(({ day }) => currentWeek == moment(day).isoWeek())
+      );
     } catch (err) {
       next(err);
     }
